66055bcd8b400d29a85df2e8f96901062a169e1f
thiagoduutra <thiagoduutra01@gmail.com>
2022-07-25 15:32:54 -0300

    adicionar historico

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+const resultInput = document.querySelector("#result");
+const buttonResult = document.querySelector(".btnEqual");
+const historic = document.querySelector(".historic");
+const buttonClear = document.querySelector("#btnClear");
+
 function insert(num) {
   let numero = document.getElementById("result").value;
   document.getElementById("result").value = numero + num;
@@ -13,3 +18,25 @@ function calculo() {
     document.getElementById("result").value = eval(result);
   }
 }
+
+buttonClear.addEventListener("click", (e) => {
+  e.preventDefault();
+  const items = document.querySelector(".historic");
+  const item = items.querySelector("li");
+
+  item.remove();
+});
+
+buttonResult.addEventListener("click", (e) => {
+  e.preventDefault();
+  const resultValue = resultInput.value;
+  const li = document.createElement("li");
+
+  if (resultInput.value !== "") {
+    li.classList = "historic";
+    li.innerHTML = `Resultado: ${resultValue}`;
+  }
+
+  historic.appendChild(li);
+  resultInput.value = "";
+});
